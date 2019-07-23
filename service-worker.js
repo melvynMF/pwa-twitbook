3924f9b15aa594b7da8e0a26c25f71667b9fee6d
joris loemba <jorisloemba@gmail.com>
2019-07-23 16:23:35 +0200

    SW

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,69 @@
 console.log('Service worker loaded');
 
 
-   const PRECACHE = 'precache-v1';
-   const RUNTIME = 'runtime';
-   
-   // A list of local resources we always want to be cached.
-   const PRECACHE_URLS = [
-     'index.html',
-     './src/assets/images/1f680.png',
-     './src/components/data/chat-auth.js',
-     './src/components/data/chat-data.js',
-     './src/components/data/chat-login.js',
-     './src/components/data/chat-profile.js',
-     './src/components/data/chat-chat-store.js',
-     './src/components/layout/navigation/chat-header.js',
-     './src/components/chat-app.js'
-   ];
-   
-   // The install handler takes care of precaching the resources we always need.
-   self.addEventListener('install', event => {
-     event.waitUntil(
-       caches.open(PRECACHE)
-         .then(cache => cache.addAll(PRECACHE_URLS))
-         .then(self.skipWaiting())
-     );
-   });
+ // Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+ // A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  '/index.html',
+  '/', // Alias for index.html
+  'src/assets',
+  'src/js/assets/images/1f680.png',
+  'src/js/components/data/chat-auth.js',
+  'src/js/components/data/chat-data.js',
+  'src/js/components/data/chat-login.js',
+  'src/js/components/data/chat-store.js',
+  'src/js/components/layout/navigation/chat-header.js'
+];
+
+ // The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+ // The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+ // The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+         return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+}); 
